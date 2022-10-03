0170c7197b05d0f88a959dfccb48cc628ec4c8f7
BaotrungCode <nguyenbaotrung0607@gmail.com>
2022-10-03 23:08:05 +0700

    high commit

--- login.php
+++ login.php
@@ -8,6 +8,7 @@
 <form action='login.php' class="dangnhap" method='POST'> 
 Tên đăng nhập : <input type='text' name='username' /> 
 Mật khẩu : <input type='password' name='password' /> 
+Phone: <input type='password' name='password' /> 
 <input type='submit' class="button" name="dangnhap" value='Đăng nhập' /> 
 <a href='D:\XAMPP\htdocs\dangky\register.php' title='Đăng ký'>Đăng ký</a> 
 <?php require 'xuly.php';?> 
